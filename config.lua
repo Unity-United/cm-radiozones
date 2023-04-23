2a89d20b4ecef55467489285076ec5f410024c1c
CloakedManz <102273984+CloakedManz@users.noreply.github.com>
2023-04-23 16:02:35 -0400

    Initial commit

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+Config.RadioDeadZones = {
+    ["sandycanyon"] = {
+        name = "sandycanyon",
+        coords = vector3(0, 0, 0),
+        radius = 25,
+        debug = true,
+    },
+    ["mountain"] = {
+        name = "mountain",
+        coords = vector3(100, 100, 36),
+        radius = 25,
+        debug = true,
+    },
+}
